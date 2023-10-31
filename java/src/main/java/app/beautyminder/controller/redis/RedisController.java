aa5916f2b2d566a2d0e8df88aae34ded36185de1
Alfex4936 <ikr@kakao.com>
2023-10-31 20:05:14 +0900

    🌐 [API] GET latest search ranking

--- java/src/main/java/app/beautyminder/controller/redis/RedisController.java
+++ java/src/main/java/app/beautyminder/controller/redis/RedisController.java
@@ -1,6 +1,8 @@
 package app.beautyminder.controller.redis;
 
 import app.beautyminder.domain.Cosmetic;
+import app.beautyminder.domain.KeywordRank;
+import app.beautyminder.repository.KeywordRankRepository;
 import app.beautyminder.service.cosmetic.CosmeticRankService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -8,12 +10,14 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -22,6 +26,7 @@ import java.util.List;
 public class RedisController {
 
     private final CosmeticRankService cosmeticRankService;
+    private final KeywordRankRepository keywordRankRepository;
 
     @Operation(
             summary = "Get N ranked products",
@@ -34,10 +39,25 @@ public class RedisController {
                     @ApiResponse(responseCode = "200", description = "성공", content = @Content(schema = @Schema(implementation = Cosmetic.class, type = "array")))
             }
     )
-    @GetMapping("/top")
+    @GetMapping("/top/cosmetics")
     public ResponseEntity<List<Cosmetic>> getTopRankedCosmetics(
             @RequestParam(defaultValue = "10") int size) {
         List<Cosmetic> topRankedCosmetics = cosmeticRankService.getTopRankedCosmetics(size);
         return ResponseEntity.ok(topRankedCosmetics);
+    }
+
+    @Operation(
+            summary = "Get top 10 keywords",
+            description = "가장 최근의 검색어 랭킹을 불러옵니다.",
+            tags = {"Redis Operations"},
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "성공", content = @Content(schema = @Schema(implementation = String.class, type = "array")))
+            }
+    )
+    @GetMapping("/top/keywords")
+    public ResponseEntity<List<String>> getTopRankedKeywords() {
+        return keywordRankRepository.findTopByOrderByCreatedAtDesc()
+                .map(rank -> ResponseEntity.ok(rank.getRankings()))
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).body(Collections.emptyList()));
     }
 }
--- java/src/main/java/app/beautyminder/repository/KeywordRankRepository.java
+++ java/src/main/java/app/beautyminder/repository/KeywordRankRepository.java
@@ -3,6 +3,9 @@ package app.beautyminder.repository;
 import app.beautyminder.domain.KeywordRank;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface KeywordRankRepository extends MongoRepository<KeywordRank, String> {
+import java.util.Optional;
 
+public interface KeywordRankRepository extends MongoRepository<KeywordRank, String> {
+    // Find the latest KeywordRank by sorting the createdAt field in descending order and limit the result to 1
+    Optional<KeywordRank> findTopByOrderByCreatedAtDesc();
 }
