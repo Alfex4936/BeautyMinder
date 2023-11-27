6673d241630bffcc613d19d081ec865db82f52ae
LeeZEun <je990420@ajou.ac.kr>
2023-11-27 04:47:58 +0000

    [cherrypicked] Update: Edit Splash Page

--- frontend/lib/pages/start/splash_page.dart
+++ frontend/lib/pages/start/splash_page.dart
@@ -62,34 +62,6 @@ class _SplashPageState extends State<SplashPage> {
     );
   }
 
-  // Widget _label() {
-  //   return Container(
-  //       margin: EdgeInsets.only(top: 40, bottom: 20),
-  //       child: Column(
-  //         children: <Widget>[
-  //           Text(
-  //             'Quick login with Touch ID',
-  //             style: TextStyle(color: Colors.white, fontSize: 17),
-  //           ),
-  //           SizedBox(
-  //             height: 20,
-  //           ),
-  //           Icon(Icons.fingerprint, size: 90, color: Colors.white),
-  //           SizedBox(
-  //             height: 20,
-  //           ),
-  //           Text(
-  //             'Touch ID',
-  //             style: TextStyle(
-  //               color: Colors.white,
-  //               fontSize: 15,
-  //               decoration: TextDecoration.underline,
-  //             ),
-  //           ),
-  //         ],
-  //       ));
-  // }
-
   Widget _title() {
     return RichText(
       textAlign: TextAlign.center,
--- frontend/lib/pages/start/welcome_page.dart
+++ frontend/lib/pages/start/welcome_page.dart
@@ -12,11 +12,11 @@ class WelcomePage extends StatefulWidget {
 }
 
 class _WelcomePageState extends State<WelcomePage> {
-
   Widget _title() {
-    return RichText(
-      textAlign: TextAlign.center,
-      text: TextSpan(
+    return Center(
+      child: RichText(
+        textAlign: TextAlign.center,
+        text: TextSpan(
           text: 'Beauty',
           style: TextStyle(
             fontFamily: 'Oswald',
@@ -29,39 +29,16 @@ class _WelcomePageState extends State<WelcomePage> {
               text: 'Minder',
               style: TextStyle(color: Color(0xfffe9738), fontSize: 30),
             ),
-          ]),
+          ],
+        ),
+      ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:SingleChildScrollView(
-        child:Container(
-          padding: EdgeInsets.symmetric(horizontal: 20),
-          height: MediaQuery.of(context).size.height,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(5)),
-              boxShadow: <BoxShadow>[
-                BoxShadow(
-                    color: Colors.grey.shade200,
-                    offset: Offset(2, 4),
-                    blurRadius: 5,
-                    spreadRadius: 2)
-              ],
-              gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: [Color(0xffffffff), Color(0xffffffff)])),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              _title(),
-            ],
-          ),
-        ),
-      ),
+      body: _title(),
     );
   }
 }
